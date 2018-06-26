88fddcdf464946cfa6af8bdf302c3d012dd5623d
nitesh <nit.it.esh@gmail.com>
2018-06-26 09:03:43 +0530

    nav

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,8 +7,21 @@ import logo from '/Users/nitesh/my-app/src/assets/images/final.png'
 export default class Header extends React.Component {
     render(){
         return (
-            <header className="App-header">
+            /*<header className="App-header">
+            <nav className="Home">Home</nav>
+            <nav className="About">About</nav>
             <img src= {logo} className= "App-logo" alt="logo" />
+            <nav className="Travelwithme">Travel with me</nav>
+            <nav className="Contact">Contact</nav>
+            </header>*/
+            <header className="App-header">
+            <ul className="nav" role="navigation">
+            <li><i className="fa fa-home"></i>Home</li>
+            <li><i className="fa fa-bell-o"></i>About</li>
+            <img src= {logo} className= "App-logo" alt="logo" />
+            <li><i className="fa fa-envelope-o"></i>Travel with me</li>
+            <li><i className="fa fa-twitter"></i>Contact</li>
+            </ul>
             </header>
         );
     }
