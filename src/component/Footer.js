8db895a66eb9f0d7ddc1e0702309a106891d691f
niteshranjan7 <32736981+niteshranjan7@users.noreply.github.com>
2018-07-23 16:31:17 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,7 +4,7 @@ import '/Users/nitesh/my-app/src/App.css';
 export default class Footer extends React.Component {
     render(){
         return (
-            <footer className="App-footer">footer</footer>
+            <footer className="App-footer">Ⓒ 2018 Nitesh Ranjan Photography. All rights reserved.</footer>
         );
     }
 }
